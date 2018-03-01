c3a7d1d268fabf7cc7454048bf7ba308a8485788
Andrew Locke <alocke12992@gmail.com>
2018-02-28 20:52:36 -0700

    Create Departments

--- app/controllers/depts_controller.rb
+++ app/controllers/depts_controller.rb
@@ -1,10 +1,30 @@
 class DeptsController < ApplicationController
   def index
+    @depts = Dept.all 
   end
 
   def new
+    @dept = Dept.new 
   end
+
+  def create 
+    @dept = Dept.create(dept_params)
+
+    if @dept.save
+      redirect_to root_path
+    else 
+      render :new 
+    end 
+
+  end 
+
+  
 
   def edit
   end
+
+  private 
+    def dept_params 
+      params.require(:dept).permit(:name)
+    end
 end
